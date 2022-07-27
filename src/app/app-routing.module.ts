740071c57d17c7541b6468324b899fd74ea2abc8
segfault0x61 <torresj1997@outlook.com>
2022-07-26 20:38:56 -0500

    add projects section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
-import { EducationComponent } from './education/education.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ProjectsComponent } from './projects/projects.component';
 
@@ -18,10 +17,6 @@ const routes: Routes = [
   {
     path: 'projects',
     component: ProjectsComponent,
-  },
-  {
-    path: 'education',
-    component: EducationComponent,
   },
   {
     path: 'contact',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
-import { EducationComponent } from './education/education.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ProjectsComponent } from './projects/projects.component';
@@ -15,7 +14,6 @@ import { SnackBarComponent } from './snack-bar/snack-bar.component';
   declarations: [
     AppComponent,
     ContactComponent,
-    EducationComponent,
     HomePageComponent,
     NavBarComponent,
     ProjectsComponent,
